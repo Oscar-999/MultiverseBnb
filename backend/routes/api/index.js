d1a7c45975f8b0c4dbb85eff7d06753b42f08e53
oscar <oscarvac74@gmail.com>
2024-03-06 20:10:29 -0600

    deletespotimage

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,15 +1,16 @@
 const router = require("express").Router();
 const sessionRouter = require("./session.js");
-const spotRouter = require("./spots.js")
+const spotRouter = require("./spots.js");
 const usersRouter = require("./users.js");
+const spotImagesRouter = require("./spot-images.js");
 const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser);
 
 router.use("/session", sessionRouter);
-router.use("/spots", spotRouter)
 router.use("/users", usersRouter);
-
+router.use("/spots", spotRouter);
+router.use("/spot-images", spotImagesRouter);
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
 });
