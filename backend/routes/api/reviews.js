bc2fabf83dd9290db7c50d7cdac51d649bb2d58d
oscar <oscarvac74@gmail.com>
2024-03-07 16:15:48 -0600

    currentuser

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,27 +1,85 @@
 const express = require("express");
 const { requireAuth } = require("../../utils/auth");
-const { Review } = require("../../db/models");
+const {
+  Review,
+  User,
+  SpotImage,
+  Spot,
+  ReviewImage,
+} = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
+const { where } = require("sequelize");
 const router = express.Router();
 
 const validateReviewEdit = [
-  check('review')
-  .exists({checkFalsy: true}),
-  check('stars')
-  .isInt({min: 1, max: 5})
-  .withMessage("Stars must be an integer from 1 to 5"),
-  handleValidationErrors
+  check("review").exists({ checkFalsy: true }),
+  check("stars")
+    .isInt({ min: 1, max: 5 })
+    .withMessage("Stars must be an integer from 1 to 5"),
+  handleValidationErrors,
 ];
 
 //Current User Reviews
-router.get("/current", requireAuth, (req,res) => {
-  
-})
+router.get("/current", requireAuth, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const reviews = await Review.findAll({
+      where: {
+        userId,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ["id", "firstName", "lastName"],
+        },
+        {
+          model: Spot,
+          attributes: [
+            "id",
+            "ownerId",
+            "address",
+            "city",
+            "state",
+            "country",
+            "lat",
+            "lng",
+            "name",
+            "price",
+          ],
+          include: [
+            {
+              model: SpotImage,
+              attributes: ["id", "url"],
+              where: { preview: true },
+              required: false,
+            },
+          ],
+        },
+        {
+          model: ReviewImage,
+          attributes: ["id", "url"],
+        },
+      ],
+    });
 
+    const formattedReviews = reviews.map((review) => {
+      const formatReview = review.toJSON();
+      const spotImages = formatReview.Spot.SpotImages;
+      formatReview.Spot.previewImage =
+        spotImages.length > 0 ? spotImages[0].url : null;
+      delete formatReview.Spot.SpotImages;
+
+      return formatReview;
+    });
+    return res.status(200).json({ Reviews: formattedReviews });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server error" });
+  }
+});
 
 //Edit a review
-router.put("/:reviewId", requireAuth,validateReviewEdit,  async (req, res) => {
+router.put("/:reviewId", requireAuth, validateReviewEdit, async (req, res) => {
   try {
     const userId = req.user.id;
     const { stars, review } = req.body;
@@ -41,9 +99,8 @@ router.put("/:reviewId", requireAuth,validateReviewEdit,  async (req, res) => {
     reviewEdit.review = review;
     reviewEdit.stars = stars;
 
-    await reviewEdit.save()
-    return res.status(300).json(reviewEdit)
-
+    await reviewEdit.save();
+    return res.status(300).json(reviewEdit);
   } catch (error) {
     return res.status(500).json({ message: "Internal Server Error" });
   }
