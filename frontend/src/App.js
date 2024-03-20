42107d3100002f91c47bde9ffeafa9b035d55a2f
oscar <oscarvac74@gmail.com>
2024-03-20 11:13:15 -0500

    edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,9 @@ import Navigation from "./components/Navigation";
 import LandingPage from "./components/LandingPage";
 import SpotPage from "./components/Spots/SpotsPage";
 import ManageSpots from "./components/Spots/ManageSpots/ManageSpots";
-import ManageReviews from './components/Reviews/ManageReviews/EditReview'
+import ManageReviews from "./components/Reviews/ManageReviews/EditReview";
 import CreateSpot from "./components/Spots/ManageSpots/CreateSpot/CreateSpot";
+import EditSpot from "./components/Spots/ManageSpots/EditSpot/EditSpot";
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
@@ -25,8 +26,9 @@ function App() {
           <Route exact path={"/spots/new"} component={CreateSpot} />
           <Route exact path={"/spots/current"} component={ManageSpots} />
           <Route exact path={"/spots/:spotId"} component={SpotPage} />
-          <Route exact path={"/reviews/current"} component={ManageReviews}/>
-          </Switch>
+          <Route exact path={"/reviews/current"} component={ManageReviews} />
+          <Route exact path={"/spots/:spotId/edit"} component={EditSpot} />
+        </Switch>
       )}
     </>
   );
