7b3d9cd5bdbde90eec89bd7870b87d456a84f986
oscar <oscarvac74@gmail.com>
2024-03-19 20:14:20 -0500

    google

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,11 +4,13 @@ import sessionReducer from "./session";
 import spotReducer from "./spots";
 import reviewReducer from "./review";
 import bookingReducer from "./booking";
+import mapsReducer from "./maps";
 const rootReducer = combineReducers({
   session: sessionReducer,
   spots: spotReducer,
   reviews: reviewReducer,
   bookings: bookingReducer,
+  maps: mapsReducer,
 });
 
 let enhancer;
