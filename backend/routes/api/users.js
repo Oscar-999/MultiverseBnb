3ccd57dc4fbe6378869e0e4a1b2f8b42b80d8e57
oscar <oscarvac74@gmail.com>
2024-03-20 13:55:50 -0500

    awsremoval

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,59 +1,79 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User } = require('../../db/models');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
 const router = express.Router();
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
-const {singlePublicFileUpload, singleMulterUpload} = require('../../awsS3')
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+const { singlePublicFileUpload, singleMulterUpload } = require("../../awsS3");
 const validateSignup = [
-  check('firstName')
+  check("firstName")
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a first name.'),
-  check('lastName')
+    .withMessage("Please provide a first name."),
+  check("lastName")
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a last name.'),
-  check('email')
+    .withMessage("Please provide a last name."),
+  check("email")
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage('Please provide a valid email.'),
-  check('username')
+    .withMessage("Please provide a valid email."),
+  check("username")
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
-    .withMessage('Please provide a username with at least 4 characters.'),
-  check('username')
-    .not()
-    .isEmail()
-    .withMessage('Username cannot be an email.'),
-  check('password')
+    .withMessage("Please provide a username with at least 4 characters."),
+  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("password")
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
-    .withMessage('Password must be 6 characters or more.'),
-  handleValidationErrors
+    .withMessage("Password must be 6 characters or more."),
+  handleValidationErrors,
 ];
 // Sign up
-router.post(
-  '',
-  singleMulterUpload("profilePic"),
-  validateSignup,
-  async (req, res) => {
-    const { email, password, username, firstName, lastName } = req.body;
-    const profilePic = await singlePublicFileUpload(req.file);
-    const hashedPassword = bcrypt.hashSync(password);
+// router.post(
+//   '',
+//   singleMulterUpload("profilePic"),
+//   validateSignup,
+//   async (req, res) => {
+//     const { email, password, username, firstName, lastName } = req.body;
+//     const profilePic = await singlePublicFileUpload(req.file);
+//     const hashedPassword = bcrypt.hashSync(password);
 
-    const user = await User.create({ email, username, hashedPassword, firstName, lastName, profilePic });
-    const safeUser = {
-      id: user.id,
-      email: user.email,
-      username: user.username,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      profilePic: user.profilePic
-    };
-    await setTokenCookie(res, safeUser);
-    return res.json({
-      user: safeUser
-    });
-  }
-);
+//     const user = await User.create({ email, username, hashedPassword, firstName, lastName, profilePic });
+//     const safeUser = {
+//       id: user.id,
+//       email: user.email,
+//       username: user.username,
+//       firstName: user.firstName,
+//       lastName: user.lastName,
+//       profilePic: user.profilePic
+//     };
+//     await setTokenCookie(res, safeUser);
+//     return res.json({
+//       user: safeUser
+//     });
+//   }
+// );
+
+router.post("", validateSignup, async (req, res) => {
+  const { email, password, username, firstName, lastName } = req.body;
+  const hashedPassword = bcrypt.hashSync(password);
+  const user = await User.create({
+    email,
+    username,
+    hashedPassword,
+    firstName,
+    lastName,
+  });
+  const safeUser = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+    firstName: user.firstName,
+    lastName: user.lastName,
+  };
+  await setTokenCookie(res, safeUser);
+  return res.json({
+    user: safeUser,
+  });
+});
 module.exports = router;
